288bf648f9bf7d8cbcede6c99ff337316ba4049c
Joel <joelbmolina@gmail.com>
2017-11-11 01:28:29 -0700

    added dispatch and navigation props for state management

--- app/index.js
+++ app/index.js
@@ -1,14 +1,36 @@
-/**
- * @flow
- */
+// @flow
 
-import { StackNavigator } from 'react-navigation'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import {
+  StackNavigator,
+  addNavigationHelpers
+} from 'react-navigation'
+
 import Main from './screens/Main'
 import Second from './screens/Second'
 
-const App = StackNavigator({
+export const AppNavigator = StackNavigator({
   Home: { screen: Main },
   Second: { screen: Second },
 })
 
-export default App
+const AppWithNavigation = ({ dispatch, navigation }) => (
+  <AppNavigator
+    navigation={addNavigationHelpers({
+      dispatch,
+      state: navigation
+    })} />
+)
+
+AppWithNavigation.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  navigation: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  navigation: state.navigation
+})
+
+export default connect(mapStateToProps)(AppWithNavigation)
