f593dc64fa5f7a156db80bd742ce261dc931eb86
Joel <joelbmolina@gmail.com>
2017-11-11 01:33:53 -0700

    refactor to have state management through redux

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+// @flow
+
+import React from 'react'
 import { AppRegistry } from 'react-native'
-import App from './app/index.js'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+
+import appReducer from './app/reducers/index'
+import AppWithNavigation from './app/index'
+
+class App extends React.Component {
+  store = createStore(appReducer,
+    applyMiddleware(thunkMiddleware))
+
+  render() {
+    return (
+      <Provider store={this.store}>
+        <AppWithNavigation />
+      </Provider>
+    )
+  }
+}
 
 AppRegistry.registerComponent('gosnoose', () => App)
