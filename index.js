537f9d9b77a37e741af740be0abb534d8ae6ebd9
Joel <joelbmolina@gmail.com>
2017-11-13 21:55:12 -0700

    moved store to its own file

--- index.js
+++ index.js
@@ -3,19 +3,15 @@
 import React from 'react'
 import { AppRegistry } from 'react-native'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
 
-import appReducer from './app/reducers/index'
 import AppWithNavigation from './app/index'
+import store from './app/config/store'
 
 class App extends React.Component {
-  store = createStore(appReducer,
-    applyMiddleware(thunkMiddleware))
 
   render() {
     return (
-      <Provider store={this.store}>
+      <Provider store={store}>
         <AppWithNavigation />
       </Provider>
     )
